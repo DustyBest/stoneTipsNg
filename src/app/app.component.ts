15117d36220e6e13c0f489f045e18f400a7d2c2d
DustyBest <dustybest@gmail.com>
2019-07-11 15:23:55 -0700

    new

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'stoneTipsNg';
+  netSales:number = 1225.00;
+}
